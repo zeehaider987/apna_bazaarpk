900a60ac7b6223fe9db94d9dd45d48b8bfe41922
zeehaider987 <zeehaider987@outlook.com>
2022-07-29 19:14:12 +0500

    back button functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
+
+import 'package:apna_bazaarpk/BackEventNotifyer.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'dart:io';
+import 'package:url_launcher/url_launcher.dart';
 void main() {
   // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp( WillPopScope(child: MyApp(), onWillPop:()async{return false;}));
+  runApp( ChangeNotifierProvider(child: HomeWebView(), create:(context) => BackEventNotifyer()));
 }
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class HomeWebView extends StatefulWidget {
+  const HomeWebView({super.key});
   @override
-  State<MyApp> createState() => new _MyAppState();
+  State<HomeWebView> createState() => new _HomeWebViewState();
 }
-class _MyAppState extends State<MyApp> {
-  // final Completer<WebViewController> _controller =
-  //     Completer<WebViewController>();
+class _HomeWebViewState extends State<HomeWebView> {
+  late WebViewController _controll;
+  GlobalKey _globalKey=GlobalKey();
   @override
   void initState() {
     super.initState();
     if (Platform.isAndroid) WebView.platform = AndroidWebView();
     // initialization();
+   
   }
   @override
   void dispose() {
@@ -49,23 +54,85 @@ class _MyAppState extends State<MyApp> {
       
       debugShowCheckedModeBanner: false,
       home:WillPopScope(
-        onWillPop: ()async{
-      print("you donot go back ");
-      return await Future.value(false);
-     } ,
+        onWillPop: _onBack,
         child: Scaffold(
+          key: _globalKey,
       appBar: AppBar(
         title: Image.asset('assets/Images/appbar.png'),
         backgroundColor:Color.fromARGB(255, 34, 181, 115)
         ),
       // ignore: prefer_const_constructors
       body: WebView(
+        
+        onWebViewCreated: ( webViewController) {
+            _controll=webViewController;},
        initialUrl: 'https://apnabazaar.pk',
+       
        javascriptMode: JavascriptMode.unrestricted,
        gestureNavigationEnabled: true,
-      )
+       
+       navigationDelegate: (NavigationRequest request)async {
+          print(request.url);
+          if (request.url.contains("mailto:")) {
+            if (!await launchUrl(Uri.parse(request.url)) ){
+              throw 'Could not launch ${Uri.parse(request.url)}' ;
+              }
+            return NavigationDecision.prevent;
+          } else {
+            return NavigationDecision.navigate;
+          }
+        }
+        
+       
+
+      ),
      ),
+
       )
      );
+  }
+  
+   Future<bool> _onBack() async {
+    bool goBack;
+    var value = await _controll.canGoBack();  // check webview can go back
+    if (value) {
+     _controll.goBack(); // perform webview back operation
+      return false;
+    } else {
+      late BackEventNotifyer _notifier;
+     await showDialog(
+        context: _globalKey.currentState!.context,
+        builder: (context) => Consumer(
+       builder: (context,BackEventNotifyer event, childs) {
+         _notifier=event;
+         return new AlertDialog(
+           title: new Text(
+               'Confirmation ', style: TextStyle(color: Color.fromARGB(131, 6, 90, 38))),
+           content: new Text('Do you want exit app ? '),
+           actions: [
+             new TextButton(
+               onPressed: () {
+                 Navigator.of(context).pop(false);
+                 event.add(false);
+               },
+               child: new Text("No"), // No
+             ),
+             new TextButton(
+               onPressed: () {
+                 Navigator.of(context).pop();
+                 event.add(true);
+               },
+               child: new Text("Yes"), // Yes
+             ),
+           ],
+         );
+       }
+       )
+      );
+
+      //Navigator.pop(_globalKey.currentState!.context);
+   print("_notifier.isBack ${_notifier.isBack}");
+      return _notifier.isBack;
+    }
   }
   }
